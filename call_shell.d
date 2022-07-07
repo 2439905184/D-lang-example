aeeb8e1d6afb812046b9637d8da44dc7c7701377
小沙盒工作室-室长 <29478722+2439905184@users.noreply.github.com>
2022-07-07 02:56:45 +0000

    Update call_shell.d

--- call_shell.d
+++ call_shell.d
@@ -1,4 +1,4 @@
 import std.process;
-auto ls = executeShell("python3 FileUtil.py temp.txt");
+auto ls = executeShell("ls -l");
 if (ls.status != 0) writeln("Failed to retrieve file listing");
 else writeln(ls.output);
